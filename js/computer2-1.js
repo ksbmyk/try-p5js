ca1323b8a68b3d7e9301b6a1411de40c5bdd8d39
ksbmyk <ksbmyk@gmail.com>
2020-10-19 23:16:57 +0900

    枠線なしの指定をsetupに移行
    全体の指定なので、実行は一度でよい

--- js/computer2-1.js
+++ js/computer2-1.js
@@ -1,13 +1,13 @@
 function setup(){
   createCanvas(800, 800);
   colorMode(HSB, 1);
+  noStroke();
 }
 
 function draw(){
   for(i = 1; i < 2000; i++){
     x = random(0, 900);
     y = random(0, 900);
-    noStroke();
     c = random(0, 255);
     fill(0, 0, c);
     rect(x, y, 30, 30); 
