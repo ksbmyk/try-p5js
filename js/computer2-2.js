3f19d73e721d63b3318519ffbbf06e28208900ad
ksbmyk <ksbmyk@gmail.com>
2022-01-24 09:48:44 +0900

    円を回転させて書く

--- js/computer2-2.js
+++ js/computer2-2.js
@@ -6,4 +6,12 @@ function setup(){
 
 function draw(){
   noLoop();
+
+  pushMatrix();
+  translate(500, 500);
+  for (int i = 0; i < 6; i++) {
+    rotate(PI/3); //180/90=30度 30度ゴト6回 -> 360度
+    ellipse(200, 200, 50, 50); // 中心 200, 200, 横50, 縦50
+  }
+  popMatrix();
 }
