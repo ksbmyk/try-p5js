57a76458caea1869e1fe7d6384369d1c9118bf8c
ksbmyk <ksbmyk@gmail.com>
2022-01-24 09:56:46 +0900

    p5.js記法に変更

--- js/computer2-2.js
+++ js/computer2-2.js
@@ -7,11 +7,11 @@ function setup(){
 function draw(){
   noLoop();
 
-  pushMatrix();
+  push();
   translate(500, 500);
-  for (int i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) {
     rotate(PI/3); //180/90=30度 30度ゴト6回 -> 360度
     ellipse(200, 200, 50, 50); // 中心 200, 200, 横50, 縦50
   }
-  popMatrix();
+  pop();
 }
