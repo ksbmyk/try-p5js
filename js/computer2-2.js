2f257ea298fa7c311c8f525999464c065252ad68
Miyuki Koshiba <m.koshiba@everyleaf.com>
2022-01-25 09:37:53 +0900

    背景を黒、angleModeをDEGREESに、扇形を追加

--- js/computer2-2.js
+++ js/computer2-2.js
@@ -1,6 +1,8 @@
 function setup(){
   createCanvas(800, 800);
   colorMode(RGB, 256);
+  background(0);
+  angleMode(DEGREES);
 }
 
 function draw(){
@@ -9,9 +11,34 @@ function draw(){
   push();
   translate(500, 500);
   for (var i = 0; i < 6; i++) {
-    rotate(PI/3); //180/90=30度 30度ゴト6回 -> 360度
+    rotate(60); //180/90=30度 30度ゴト6回 -> 360度
     ellipse(30, 30, 45, 45); // 中心 200, 200, 横50, 縦50
   }
   ellipse(0, 0, 80, 80);
+
+  // 外周の円
+  for (var i = 0; i < 36; i++) {
+    rotate(10); //180/90=30度 30度ゴト6回 -> 360度
+    ellipse(150, 150, 20, 20); // 中心 200, 200, 横50, 縦50
+  }
   pop();
+
+  // 楕円
+  // push();
+  // translate(500, 500);
+  // for (var i = 0; i < 12; i++) {
+  //   rotate(30);
+  //   ellipse(10, 130, 10, 100);
+  // }
+  // pop();
+
+  // 扇
+  push();
+  translate(500, 500);
+  for (var i = 0; i < 12; i++) {
+    rotate(30);
+    arc(90, 130, 80, 80, 0, 90);
+  }
+  pop();
+
 }
