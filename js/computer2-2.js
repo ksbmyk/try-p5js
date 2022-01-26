5392ce3df9d86c0ba7d6b2123703b0a96fa949de
Miyuki Koshiba <m.koshiba@everyleaf.com>
2022-01-26 10:03:28 +0900

    円を増やして、線画を整える

--- js/computer2-2.js
+++ js/computer2-2.js
@@ -9,36 +9,42 @@ function draw(){
   noLoop();
 
   push();
-  translate(500, 500);
+  translate(400, 400);
+  stroke(1);
+  // 扇
+  for (var i = 0; i < 24; i++) {
+    n = 15 * i
+    //arc(x, y, w, h, start, stop)
+    arc(0, 0, 350, 350, 0 + n, 10 + n);
+  }
+
+  // 扇の上の円
+  noStroke();
+  for (var i = 0; i < 24; i++) {
+    rotate(15);
+    ellipse(175, 15, 31, 31);
+    ellipse(190, 15, 10, 10);
+  }
+
+  stroke(1);
+  // 黒円
+  fill(0);
+  ellipse(0, 0, 150, 150);
+
+  fill(255);
+  //花びら
   for (var i = 0; i < 6; i++) {
-    rotate(60); //180/90=30度 30度ゴト6回 -> 360度
-    ellipse(30, 30, 45, 45); // 中心 200, 200, 横50, 縦50
+    rotate(60);
+    ellipse(30, 30, 50, 50); // 中心 30, 30, 横50, 縦50
+    ellipse(30, 30, 45, 45);
   }
   ellipse(0, 0, 80, 80);
 
   // 外周の円
   for (var i = 0; i < 36; i++) {
-    rotate(10); //180/90=30度 30度ゴト6回 -> 360度
-    ellipse(150, 150, 20, 20); // 中心 200, 200, 横50, 縦50
+    rotate(10);
+    ellipse(155, 155, 25, 25)
+    ellipse(155, 155, 20, 20);
   }
   pop();
-
-  // 楕円
-  // push();
-  // translate(500, 500);
-  // for (var i = 0; i < 12; i++) {
-  //   rotate(30);
-  //   ellipse(10, 130, 10, 100);
-  // }
-  // pop();
-
-  // 扇
-  push();
-  translate(500, 500);
-  for (var i = 0; i < 12; i++) {
-    rotate(30);
-    arc(90, 130, 80, 80, 0, 90);
-  }
-  pop();
-
 }
