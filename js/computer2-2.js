066504fb8d1eb6d7339c0398476a301814ade5f9
Miyuki Koshiba <m.koshiba@everyleaf.com>
2023-04-27 13:19:09 +0900

    提出用

--- js/computer2-2.js
+++ js/computer2-2.js
@@ -2,101 +2,107 @@ function setup(){
   createCanvas(800, 800);
   colorMode(RGB, 256);
   background(0);
-  angleMode(DEGREES);
+  angleMode(DEGREES); // 円を0度〜360度で扱う
 }
 
 function draw(){
   noLoop();
-  // 光らせる
-  blendMode(SCREEN);
 
+  // 800*800の中心400,400を原点にする
   push();
   translate(400, 400);
-  //stroke(1);
+
+  // 光らせる
+  blendMode(SCREEN); // 重ねると色が明るくなる
+
   // 扇
-  fill("red");
+  fill(0, 150 , 255); // シアン
   for (var i = 0; i < 24; i++) {
     n = 15 * i
     for (var j = 0; j < 15; j++){
-      strokeWeight(j * 0.2);
-      stroke(200 - j, 15, 15, 100);
-      //arc(x, y, w, h, start, stop)
+      strokeWeight(j * 0.8);
+      stroke(0, 150 - j, 255, 100);
       arc(0, 0, 350, 350, 0 + n, 10 + n, PIE);
     }
   }
-  stroke(1);
-  strokeWeight(1);
+
   // 扇の上の円
   noStroke();
-  fill("blue");
+  fill(0, 0, 255); //青
   for (var i = 0; i < 24; i++) {
     rotate(15);
     for (var j = 0; j < 15; j++){
       strokeWeight(j * 0.2);
       stroke(0, 15, 255 - j, 100);
       ellipse(175, 15, 31, 31);
+    }
+    for (var j = 0; j < 15; j++){
+      strokeWeight(j * 1);
+      stroke(0, 15, 100 - j, 150);
       ellipse(190, 15, 10, 10);
     }
   }
 
+  // 外周の円
+  fill(255, 0, 0); // 赤
+  for (var i = 0; i < 12; i++) {
+    rotate(30);
+    for (var j = 0; j < 15; j++){
+      strokeWeight(j * 1);
+      stroke(100, 25 - j, 0, 100);
+      ellipse(110, 190, 25, 25);
+    }
+  }
 
-  stroke(1);
-  blendMode(BLEND);
-  // 黒円
+  // 外周の大円
+  fill(10, 0 , 128); //紫
+  for (var i = 0; i < 12; i++) {
+    rotate(30);
+    for (var j = 0; j < 15; j++){
+      strokeWeight(j * 1);
+      stroke(100, 50, 128 - j, 100);
+      ellipse(200, 200, 100, 100);
+    }
+  }
+
+  blendMode(BLEND); //色を重ねたとき上書きする
+
+  // 中心黒小円
   fill(0);
   ellipse(0, 0, 150, 150);
 
-  fill("yellow");
+  blendMode(SCREEN);
+
   //花びら
   for (var i = 0; i < 6; i++) {
     rotate(60);
+    fill(255, 255, 0); // 黄
     for (var j = 0; j < 15; j++){
-      strokeWeight(j * 0.2);
-      stroke(100, 100, 25 - j, 100); //stroke(v1, v2, v3, [alpha])
-      //ellipse(30, 30, 50, 50); // 中心 30, 30, 横50, 縦50
-      ellipse(30, 30, 45, 45);
+      strokeWeight(j * 1);
+      stroke(100, 100, 25 - j, 100);
+      ellipse(30, 30, 45, 45); //中心まわりの円
     }
   }
+  
+  // 中心円
+  fill(0, 150 , 255); 
+  blendMode(BLEND);
   ellipse(0, 0, 80, 80);
 
-  // 外周の円
-  for (var i = 0; i < 36; i++) {
-    rotate(10);
-    for (var j = 0; j < 15; j++){
-      strokeWeight(j * 0.2);
-      stroke(100, 100, 100 - j, 100);
-      ellipse(155, 155, 25, 25)
-    }
-    //ellipse(155, 155, 20, 20);
+  // 中心発光
+  blendMode(SCREEN);
+  for (var j = 0; j < 15; j++){
+    strokeWeight(j * 1);
+    stroke(0, 20 - j, 255, 100);
+    ellipse(0, 0, 100, 100);
   }
 
-// 習作エリア
-// 参考: https://note.com/deconbatch/n/nadd699e04580
+  blendMode(BLEND);
 
-    for (var i = 1; i < 15; ++i) {
-       strokeWeight(i * 0.2); //線の太さ
-       stroke(100, 100, 100, 100); //stroke(v1, v2, v3, [alpha])
-       ellipse(150, 200, 25, 25);
-    }
+  // 外側に黒い太い円を描を置き半円にする
+  noFill();
+  strokeWeight(80);
+  stroke(0, 0, 0);
+  ellipse(0, 0, 650, 650);
 
-    for (var i = 1; i < 15; ++i) {
-       strokeWeight(i * 0.2); //線の太さ
-       stroke(100, 100, 100 - i, 100); //stroke(色相, 彩度, 明るさ, [alpha])
-       ellipse(200, 200, 25, 25);
-    }
-
-    for (var i = 1; i < 15; ++i) {
-       strokeWeight(i * 0.2); //線の太さ
-       stroke(100, 100, 25-i, 100); //stroke(v1, v2, v3, [alpha])
-       ellipse(250, 200, 25, 25);
-    }
-
-fill("red");
-    // 扇を囲むような線ではなく扇のこの部分にだけしか線が引けていないから発光にならない
-    strokeWeight(i * 0.2); //線の太さ
-    //stroke(100, 100, 25, 100); //stroke(v1, v2, v3, [alpha])
-    stroke(15, 15, 255, 100);
-    arc(100, 350, 350, 350, 0 + n, 10 + n, PIE); // arc(中心x, 中心y, 円のw, 円のh, 確度start, 確度stop, [mode], [detail])
-    // mode: 定数：弧の描画方法を決定するパラメータ。 CHORD、PIE または OPEN(オプション)
-  pop();
 }
