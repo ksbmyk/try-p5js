72b4a82f7982df12cc772647a0e66fcc4ba1b093
Miyuki Koshiba <ksbmyk@gmail.com>
2023-12-03 12:11:42 +0900

    switching circle

--- js/xmas2023.js
+++ js/xmas2023.js
@@ -1,24 +1,32 @@
 // for 『NEORT』×『Pop Up! YohaS』 Digital Art Show
 // https://neort.io/challenge/cl0tss4n70rtc72b48v0
 
+let circles = [];
+let numCircles = 800;
+
 function setup() {
     createCanvas(1920, 970);
-    colorMode(RGB);
-    background(0, 30, 70);
-    blendMode(OVERLAY);
-    noLoop();
+
+    for (let i = 0; i < numCircles; i++) {
+      let circle = {
+        x: random(width),
+        y: random(height),
+        diameter: 30,
+        alphaValue: 0,
+        speed: random(0.01, 0.03)
+      };
+      circles.push(circle);
+    }
   }
   
   function draw() {
-    for (let i = 1; i <= 1500; i++) {
-      if (i % 2 === 0) {
-        noStroke();
-        fill(170, 200, 255);
-      } else {
-        stroke(170, 200, 255);
-        fill(255, 255, 255, 100);
-      }
-      ellipse(random(0, 1920), random(0, 970), 30);
+    background(0, 30, 70);
+  
+    for (let i = 0; i < circles.length; i++) {
+      let circle = circles[i];
+      circle.alphaValue = 128 + 128 * sin(frameCount * circle.speed);
+      noStroke();
+      fill(255, 255, 255, circle.alphaValue);
+      ellipse(circle.x, circle.y, circle.diameter);
     }
   }
- 
