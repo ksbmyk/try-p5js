489edabbb1ac4d252c4788ae40ef1eca9d4ef0ad
Miyuki Koshiba <ksbmyk@gmail.com>
2023-12-03 16:24:18 +0900

    fix indent

--- js/xmas2023.js
+++ js/xmas2023.js
@@ -9,30 +9,30 @@ function setup() {
     createCanvas(1920, 970);
 
     for (let i = 0; i < numCircles; i++) {
-      let circle = {
-        x: random(width),
-        y: random(height),
-        color: colorPalette[floor(random(colorPalette.length))],
-        diameter: 30,
-        alphaValue: 0,
-        speed: random(0.01, 0.03)
-      };
-      circles.push(circle);
+        let circle = {
+            x: random(width),
+            y: random(height),
+            color: colorPalette[floor(random(colorPalette.length))],
+            diameter: 30,
+            alphaValue: 0,
+            speed: random(0.01, 0.03)
+        };
+        circles.push(circle);
     }
   }
   
-  function draw() {
+function draw() {
     background(0, 30, 70);
     for (let i = 0; i < circles.length; i++) {
-      let circle = circles[i];
-      circle.alphaValue = 128 + 128 * sin(frameCount * circle.speed);
-      noStroke();
+        let circle = circles[i];
+        circle.alphaValue = 128 + 128 * sin(frameCount * circle.speed);
+        noStroke();
 
-      let c = color(circle.color);
-      c.setAlpha(circle.alphaValue);
-      fill(c);
+        let c = color(circle.color);
+        c.setAlpha(circle.alphaValue);
+        fill(c);
 
-      ellipse(circle.x, circle.y, circle.diameter);
+        ellipse(circle.x, circle.y, circle.diameter);
     }
-  }
+}
  
