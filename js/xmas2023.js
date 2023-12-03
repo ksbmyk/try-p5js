bdfdcddb7bcd4ff8b16b6bfa8ae2bf985dc48ed1
Miyuki Koshiba <ksbmyk@gmail.com>
2023-12-03 11:16:04 +0900

    add base

---
+++ js/xmas2023.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(800, 800);
+    colorMode(RGB);
+    background(0, 30, 70);
+    blendMode(OVERLAY);
+      noLoop();
+  }
+  
+  function draw() {
+    for (let i = 1; i <= 1500; i++) {
+      if (i % 2 === 0) {
+        noStroke();
+        fill(170, 200, 255);
+      } else {
+        stroke(170, 200, 255);
+        fill(255, 255, 255, 100);
+      }
+      ellipse(random(0, 900), random(0, 900), 30);
+    }
+  }
+  
