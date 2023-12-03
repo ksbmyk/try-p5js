807f32ee196218f3ea1f76dd2aad730237fac4aa
Miyuki Koshiba <ksbmyk@gmail.com>
2023-12-03 16:22:19 +0900

    add color palette

--- js/xmas2023.js
+++ js/xmas2023.js
@@ -3,6 +3,7 @@
 
 let circles = [];
 let numCircles = 800;
+let colorPalette = ["#ffffff", "#ff0000", "#ffff00", "#0000ff", "#80ff00"];
 
 function setup() {
     createCanvas(1920, 970);
@@ -11,6 +12,7 @@ function setup() {
       let circle = {
         x: random(width),
         y: random(height),
+        color: colorPalette[floor(random(colorPalette.length))],
         diameter: 30,
         alphaValue: 0,
         speed: random(0.01, 0.03)
@@ -21,12 +23,16 @@ function setup() {
   
   function draw() {
     background(0, 30, 70);
-  
     for (let i = 0; i < circles.length; i++) {
       let circle = circles[i];
       circle.alphaValue = 128 + 128 * sin(frameCount * circle.speed);
       noStroke();
-      fill(255, 255, 255, circle.alphaValue);
+
+      let c = color(circle.color);
+      c.setAlpha(circle.alphaValue);
+      fill(c);
+
       ellipse(circle.x, circle.y, circle.diameter);
     }
   }
+ 
